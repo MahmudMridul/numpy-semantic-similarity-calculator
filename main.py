d8ea1939a4a190e1f8f8eff3a80a8079c7eb722e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-07-13 14:02:18 +0600

    feat: implement sentence reader

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+
+
+def read_sentences(file_path: str) -> list[str]:
+    try: 
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+        return lines
+    except FileNotFoundError as e:
+        print(e)
+        return []
+    except OSError as e:
+        print(e)
+        return []
+    
+
 def main():
-    print("Hello from numpy-semantic-similarity-calculator!")
+    FILE = 'sentences.txt'
+    sentences = read_sentences(file_path=FILE)
+    print(sentences)    
 
 
 if __name__ == "__main__":
